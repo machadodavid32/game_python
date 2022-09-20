7f252442d1b6d2aa60cbb5a11870096a8a9d0bf2
David Machado <machadodavid32@gmail.com>
2022-09-19 21:02:16 -0300

    Começando o projeto game. Criando arquivos teste, game, calcular

---
+++ game.py
@@ -0,0 +1,15 @@
+from models.calcular import Calcular
+
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+
+def jogar(pontos: int) -> None:
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
